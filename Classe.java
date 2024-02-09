e2ff4a7b6444028de7742ec4314a56ad0d373e46
Mouaadlahmani <157011355+Mouaadlahmani@users.noreply.github.com>
2024-02-09 16:55:32 +0100

    Add files via upload

---
+++ Classe.java
@@ -0,0 +1,64 @@
+public class Classe {
+    private int numero;
+    private String nom;
+    private int effectif;
+    public Apprenant list;
+
+    //Method pour AjouterClasse
+    public void ajouterC(){
+
+    }
+    
+    //Method pour AfficherClasse
+    public void AfficherC(){
+
+    }
+
+    //Method pour ModifierClasse
+    public void ModifierC(){
+
+    }
+
+    //Method pour FiltrerApprenantParClasse
+    public void Filtrer(){
+
+    }
+
+    //Method pour SuprimerClasse
+    public void SupprimerC(){
+
+    }
+    
+    //Getters And Setters
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getEffectif() {
+        return this.effectif;
+    }
+
+    public void setEffectif(int effectif) {
+        this.effectif = effectif;
+    }
+
+    public Apprenant getList() {
+        return this.list;
+    }
+
+    public void setList(Apprenant list) {
+        this.list = list;
+    }
+}
